1443d30db86f14ed82e05b882315af4c92248858
Legion <xdjlee@gmail.com>
2017-10-05 16:57:14 -0700

    update js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,6 +11,8 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 $("#submit").on("click", function() {
+  event.preventDefault();
+
   var name = $("#name").val().trim();
   var dest = $("#dest").val().trim();
   var first = $("#first").val().trim();
@@ -27,13 +29,29 @@ $("#submit").on("click", function() {
 database.ref().on("child_added", function(snapshot) {
   var name = $("<td>").text(snapshot.val().name);
   var dest = $("<td>").text(snapshot.val().dest);
-  var freq = $("<td>").text(snapshot.val().freq);
-  
-  var first = snapshot.val().first;
-  console.log(first)
-  var now = moment().format("LT");
-  console.log(now)
-  console.log(moment(first).add(10, "mins"))
+  var freq = snapshot.val().freq;
+  var frequency = $("<td>").text(freq);
+
+  var first = snapshot.val().first; //first train time ex. 00:00
+  var firstArr = first.split(":") //array ex. [00, 00]
+  var trainTime = moment().hours(firstArr[0]).minutes(firstArr[1]).seconds(0); //calls moment() and changes hours/minutes to first train time
+
+  var diff = moment().diff(moment(trainTime), "minutes");
+
+  while (diff > 0) {//when diff becomes < 0, it means that train has not arrived yet, so we will use that as 'next train' time
+    trainTime = moment(trainTime).add(freq, "minutes");//increment train time by the frequency
+    diff = moment().diff(moment(trainTime), "minutes");//rechecks the difference between now and train
+  }
+
+  var next = moment(trainTime).format("hh:mm");//gets train time from above and format it
+  next = $("<td>").text(next);
+
+  var away = moment(trainTime).diff(moment(), "minutes");//difference between next train and now
+  away = $("<td>").text(away);
+
+  var newTR = $("<tr>");
+  newTR.append(name, dest, frequency, next, away)
+  $("#addSchedule").append(newTR);
 })
 
 
